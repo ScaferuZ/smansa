23cba9d19f93d182a2a81479623873a3a4675cbe
Endra Lazuardi <endrafruz@gmail.com>
2021-04-30 05:41:09 +0700

    responsive navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,16 +1,45 @@
 import React from "react";
 import Logo from "../img/logo.svg";
+import { Link } from "react-router-dom";
+import { AiOutlineDown, AiOutlineMenu } from "react-icons/ai";
 
 function Navbar() {
   return (
-    <nav className="flex justify-between max-w-7xl mx-auto py-5 px-4 bg-gray-400">
+    <nav className="flex justify-between mx-auto py-5 px-3 md:px-32 font-Poppins bg-main text-white">
       {/* nav kiri */}
-      <div className="flex items-center space-x-6">
-        <img src={Logo} alt="logo smansa" />
-        <h1>SMAN 1 BANJARBARU</h1>
+      <div className="font-bold text-lg">
+        <Link to="/" className="flex items-center space-x-6">
+          <img src={Logo} alt="logo smansa" />
+          <h1>SMA NEGERI 1 BANJARBARU</h1>
+        </Link>
       </div>
       {/*nav kanan  */}
-      <div>kanan</div>
+      <div className="hidden lg:flex items-center space-x-6 text-sm font-semibold">
+        <Link to="/" className="nav__secondary">
+          HOME
+        </Link>
+        <Link to="/" className="nav__secondary">
+          PROFIL SEKOLAH
+        </Link>
+        <div className="flex items-center space-x-2">
+          <Link to="/" className="nav__secondary">
+            AKADEMIK
+          </Link>
+          <AiOutlineDown />
+        </div>
+        <Link to="/" className="nav__secondary">
+          BERITA
+        </Link>
+        <Link to="/" className="nav__secondary">
+          KONTAK
+        </Link>
+      </div>
+      {/* mobile button */}
+      <div className="md:hidden flex">
+        <button className="focus:outline-none">
+          <AiOutlineMenu />
+        </button>
+      </div>
     </nav>
   );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,15 @@ module.exports = {
   purge: ["./src/**/*.{js,jsx,ts,tsx}", "./public/index.html"],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        Poppins: ["Poppins"],
+      },
+      colors: {
+        main: "#161824",
+        secondary: "#F9BF3B",
+      },
+    },
   },
   variants: {
     extend: {},
